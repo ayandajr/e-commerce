2248fc17772885e8c32c4a789dcb2ab1e3f7f3b6
Ayanda Vilakazi <ayandaxjr@icloud.com>
2023-12-08 15:23:33 +0200

    fourth commit.

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
   ignorePatterns: ['**/payload-types.ts'],
   plugins: ['prettier'],
   rules: {
-    "prettier/prettier": ['error', {endOfLine : 'never'}],
+    "prettier/prettier": ['warn', {endOfLine : 'auto'}],
     'no-console': 'off'
   }
 }
