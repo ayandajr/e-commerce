02dc8d9db5d53148bd24a01842368ea498e05ece
Ayanda Vilakazi <ayandaxjr@icloud.com>
2023-12-09 16:18:41 +0200

    final commit.

--- src/payload/payload.config.ts
+++ src/payload/payload.config.ts
@@ -19,7 +19,7 @@ import { Pages } from './collections/Pages'
 import Products from './collections/Products'
 import Users from './collections/Users'
 import BeforeDashboard from './components/BeforeDashboard'
-import BeforeLogin from './components/BeforeLogin'
+// import BeforeLogin from './components/BeforeLogin'
 import { createPaymentIntent } from './endpoints/create-payment-intent'
 import { customersProxy } from './endpoints/customers'
 import { productsProxy } from './endpoints/products'
@@ -47,7 +47,7 @@ export default buildConfig({
     components: {
       // The `BeforeLogin` component renders a message that you see while logging into your admin panel.
       // Feel free to delete this at any time. Simply remove the line below and the import `BeforeLogin` statement on line 15.
-      beforeLogin: [BeforeLogin],
+      // beforeLogin: [BeforeLogin],
       // The `BeforeDashboard` component renders the 'welcome' block that you see after logging into your admin panel.
       // Feel free to delete this at any time. Simply remove the line below and the import `BeforeDashboard` statement on line 15.
       beforeDashboard: [BeforeDashboard],
@@ -112,13 +112,13 @@ export default buildConfig({
       method: 'get',
       handler: productsProxy,
     },
-    // The seed endpoint is used to populate the database with some example data
-    // You should delete this endpoint before deploying your site to production
-    {
-      path: '/seed',
-      method: 'get',
-      handler: seed,
-    },
+    // // The seed endpoint is used to populate the database with some example data
+    // // You should delete this endpoint before deploying your site to production
+    // {
+    //   path: '/seed',
+    //   method: 'get',
+    //   handler: seed,
+    // },
   ],
   plugins: [
     // formBuilder({}),
