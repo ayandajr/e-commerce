eeba69046f3ef204cc8df7e6a76f5483d475b27a
Ayanda Vilakazi <ayandaxjr@icloud.com>
2023-12-09 14:34:18 +0200

    final commit.

--- src/app/(pages)/account/page.tsx
+++ src/app/(pages)/account/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Metadata } from 'next'
+import  { Metadata }  from 'next'
 
 import { mergeOpenGraph } from '../../_utilities/mergeOpenGraph'
 import AccountForm from './AccountForm'
